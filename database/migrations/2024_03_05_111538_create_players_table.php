d2971fc11d8be790698948b2742e01894cf9b809
Kritika80517 <kritikakumari80517@gmail.com>
2024-03-16 11:57:26 +0530

    players

--- database/migrations/2024_03_05_111538_create_players_table.php
+++ database/migrations/2024_03_05_111538_create_players_table.php
@@ -15,16 +15,15 @@ return new class extends Migration
         Schema::create('players', function (Blueprint $table) {
             $table->id();
             $table->longText('team_id')->nullable();
+            $table->longText('match_id')->nullable();
             $table->string('name');
             $table->date('dob');
             $table->string('country');
             $table->string('playing_role');
             $table->string('batting_style');
             $table->string('bowling_style');
-            $table->string('batting');
-            $table->string('bowling');
             $table->string('image');
-            $table->longText('profile');
+            $table->longText('about');
             $table->timestamps();
         });
     }
